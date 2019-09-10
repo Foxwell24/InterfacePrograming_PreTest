a1811a6862881c9d78fe7d279b808d4c6d2e06e4
Kael Foxwell <kaefoxewe@gmail.com>
2019-09-10 15:03:14 +1000

    Step 5.x
    left

--- TheMovieDB/src/app/move-starter.ts
+++ TheMovieDB/src/app/move-starter.ts
@@ -2,7 +2,7 @@ import { Movie } from './movies';
 
 export const MOVIE: Movie[] = [
   {
-      Title: "Bobs", 
+    Title: "Bobs", 
     Rating: 10, 
     ShortDescription: "boooooooob bob bob bob", 
     Runtime: 200,
